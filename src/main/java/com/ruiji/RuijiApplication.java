823bcd323370d3f97152b01929459b6afeba800a
夏 <whitenight2xia@gmail.com>
2023-10-24 20:37:50 +0800

    config project

--- src/main/java/com/ruiji/RuijiApplication.java
+++ src/main/java/com/ruiji/RuijiApplication.java
@@ -1,13 +1,16 @@
 package com.ruiji;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RuijiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RuijiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RuijiApplication.class, args);
+        log.info("spring boot is ok");
+    }
 
 }
